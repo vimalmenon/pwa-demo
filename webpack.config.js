a229a6335b825ff98a3b91b26e5b2481becacb02
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2022-02-24 19:29:13 +0800

    Working on webpack

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require("path");
+const webpack  = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: {
+        main : path.resolve(__dirname, "./src/index.tsx")        
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.(css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.ts(x?)$/,
+                exclude: /node_modules/,
+                use: [
+                    { 
+                        loader: 'babel-loader'
+                    },
+                    {
+                        loader: "ts-loader",
+                    }
+                ]
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg|jpe?g|png|gif|svg|jpg|pdf|ico|webmanifest)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.ts(x?)$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({            
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new HtmlWebpackPlugin({
+            title: "Application Title",
+            hash: false,
+            filename : './index.html',
+            template: './index.html',
+            inject: true,
+        }),
+        new webpack.DefinePlugin({
+        })
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".scss"],
+        alias : {
+        }
+    }
+};
